b5f2f1555745aa66f65a629af45c35822ffd99e5
Andras <andras@kinvey.com>
2017-07-09 16:45:14 -0400

    working version; 0.1.0

---
+++ qerror.js
@@ -0,0 +1,134 @@
+/**
+ * fatal error handler, to clean up before exiting
+ *
+ * 2017-07-07 - AR.
+ */
+
+'use strict';
+
+var util = require('util');
+
+function SignalError( message ) {
+    Error.call(this, message);
+    Error.captureStackTrace(this, SignalError);
+    this.message = message;
+}
+util.inherits(SignalError, Error);
+
+// export the fatal error handler singleton
+var qerror = module.exports = new Qerror().install();
+
+function Qerror( ) {
+    this.handler = false;               // shutdown handler(err, cb), TBD set by user
+    this.timeout = 30000;               // how long to wait for the shutdown handler
+    this.alert = defaultAlert;          // set to false to not write notices about fatal errors
+    this.install = install;
+    this.uninstall = uninstall;
+
+    this._exiting = false;
+    this._timeout = false;
+
+    this._handleFatalError = function _handleFatalError( err, message ) {
+        message = message || err.message;
+        if (this.alert) this.alert(err, message);
+        if (this._exiting) {
+            process.stdout.write("already exiting, error ignored:" + message + ": " + err + "\n");
+            return;
+        }
+        this._exiting = true;
+        var self = this;
+        var shutdownTimer;
+        function timeoutHandler( ) {
+            process.removeListener('uncaughtException', onUncaught);
+            self._timeout = true;
+            throw new Error("qerror: shutdown handler took more than " + self.timeout + " ms");
+        }
+        function errorHandlerCallback( err2 ) {
+            clearTimeout(shutdownTimer);
+            process.removeListener('uncaughtException', onUncaught);
+            // NOTE: an error thrown from inside the uncaughtException handler
+            // is not relayed to the other uncaughtException handlers.
+            // However, signals must be rethrown from the current stack.
+            // As a workaround, invoke them ourselves, to not run any code past this error.
+            // Calling the other handlers is needed for our unit tests to work.
+            if (!self._timeout) {
+                if (err instanceof SignalError) throw err;
+                else {
+                    var funcs = process.listeners('uncaughtException');
+                    for (var i=0; i<funcs.length; i++) funcs[i](err);
+                }
+            }
+        }
+        var shutdownTimer = setTimeout(timeoutHandler, this.timeout);
+        var handler = this.handler || defaultHandler;
+        handler(err, errorHandlerCallback);
+    }
+
+    // for testing:
+    this.SignalError = SignalError;
+    this.reset = function reset( ) {
+        this.uninstall();
+        this.handler = false;
+        this.timeout = 30000;
+        this._exiting = false;
+        this._timeout = false;
+        this.install();
+    }
+}
+
+function defaultAlert( err, msg ) {
+    console.log("%s -- fatal error: %s", new Date().toISOString(), msg);
+}
+
+function defaultHandler( err, cb ) {
+    cb();
+}
+
+function onUncaught( err ) {
+console.log("AR: sig uncaught");
+    qerror._handleFatalError(err, 'uncaught exception');
+}
+
+function onHup( ) {
+    // HUP is not fatal if someone is handling SIGHUP
+console.log("AR: sig HUP");
+    if (process.listeners('SIGHUP').length > 1) return;
+    qerror._handleFatalError(new SignalError('SIGHUP'));
+}
+
+function onInt( ) {
+console.log("AR: sig INT");
+    qerror._handleFatalError(new SignalError('SIGINT'));
+}
+
+function onTerm( ) {
+console.log("AR: sig TERM");
+    qerror._handleFatalError(new SignalError('SIGTERM'));
+}
+
+
+function install( ) {
+    // make idempotent, remove self if already installed
+    this.uninstall();
+
+    process.on('uncaughtException', onUncaught);
+    process.on('SIGHUP', onHup);
+    process.on('SIGINT', onInt);
+    process.on('SIGTERM', onTerm);
+    return this;
+}
+
+function uninstall( ) {
+    process.removeListener('uncaughtException', onUncaught);
+    try {
+        process.removeListener('SIGHUP', onHup);
+        process.removeListener('SIGINT', onInt);
+        process.removeListener('SIGTERM', onTerm);
+    } catch (err) {
+        // cannot remove, ignore error from node v0.10
+    }
+    return this;
+}
+
+// export the pre-installed error handler singleton
+module.exports = qerror.install();
---
+++ test-qerror.js
@@ -0,0 +1,250 @@
+'use strict';
+
+var assert = require('assert');
+var util = require('util');
+var qerror = require('./');
+
+// nyc (test coverage tool) installs a SIGHUP listener, which breaks our tests
+//var existingSighupListeners = process.listeners('SIGHUP');
+process.removeAllListeners('SIGHUP');
+
+
+if (!global.setImmediate) global.setImmediate = process.nextTick;
+
+
+
+// capture all console output
+var outputLines = [];
+process.stdout.__sysWrite__ = process.stdout.write;
+process.stdout.write = function(msg, a, b) {
+    outputLines.push(msg);
+    process.stdout.__sysWrite__(msg);
+}
+
+function killSelf( sig ) {
+    // NOTE: send the kill from a setTimeout.
+    // If killed directly from inside a setImmediate, the signal
+    // bypasses the sig handler and kills the process dead.
+    setTimeout(function(){ process.kill(process.pid, sig) }, 1);
+}
+
+var testFuncs = [
+
+    // should invoke handler on uncaught error
+    function(done) {
+        var err = new Error("some uncaught error");
+        qerror.reset();
+        var called = false;
+        qerror.handler = function(err, cb) { called = err; cb() };
+        // NOTE: uncaughtException listener is not invoked when error is thrown from inside an uncaughtException handler
+        process.once('uncaughtException', function(err2) {
+            assert.equal(err2, err);
+            assert.equal(called, err);
+            setTimeout(done, 10);
+        })
+        throw err;
+    },
+
+    // should rethrow a fatal error without a handler
+    function(done) {
+        qerror.reset();
+        qerror.handler = false;
+        process.once('uncaughtException', function(err) {
+            assert.equal(err.message, 'SIGINT');
+            assert(outputLines.pop().indexOf('fatal error: SIGINT') >= 0);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGINT');
+    },
+
+    // should rethrow the error with a handler
+    function(done) {
+        qerror.reset();
+        qerror.handler = function(err, cb) { cb() };
+        process.once('uncaughtException', function(err) {
+            assert.equal(err.message, 'SIGINT');
+            assert(outputLines.pop().indexOf('fatal error: SIGINT') >= 0);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGINT');
+    },
+
+    // should omit alert if disabled
+    function(done) {
+        qerror.reset();
+        outputLines = [];
+        qerror.alert = false;
+        process.once('uncaughtException', function(err) {
+            assert(outputLines.pop().indexOf('fatal error: SIGINT') < 0);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGINT');
+    },
+
+    // should expose SignalError
+    function(done) {
+        assert.equal(typeof qerror.SignalError, 'function');
+        assert(new qerror.SignalError() instanceof Error);
+        done();
+    },
+
+    // should invoke handler on SIGINT
+    function(done) {
+        qerror.reset();
+        var called = false;
+        qerror.handler = function(err, cb) { called = true; cb() };
+        process.once('uncaughtException', function(err) {
+            assert(called);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGINT');
+    },
+
+    // should invoke handler on SIGTERM
+    function(done) {
+        qerror.reset();
+        var called = false;
+        qerror.handler = function(err, cb) { called = true; cb() };
+        process.once('uncaughtException', function(err) {
+            assert(called);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGTERM');
+    },
+
+    // should invoke handler on SIGHUP
+    function(done) {
+        qerror.reset();
+        var called = false;
+        qerror.handler = function(err, cb) { called = true; cb() };
+        process.once('uncaughtException', function(err) {
+            assert(called);
+            setTimeout(done, 10);
+        })
+        killSelf('SIGHUP');
+    },
+
+    // should not throw or invoke handler if SIGHUP already listened for
+    function(done) {
+        qerror.reset();
+        var called = false;
+        var sighupHandler;
+        process.on('SIGHUP', sighupHandler = function() {
+            // app SIGHUP handler
+        })
+        qerror.handler = function(err, cb) { called = true; cb() };
+        setTimeout(function(){
+            process.removeListener('SIGHUP', sighupHandler);
+            assert(!called);
+            setTimeout(done, 10);
+        }, 10);
+        killSelf('SIGHUP');
+    },
+
+    // should ignore second error if already shutting down
+    function(done) {
+        qerror.reset();
+        var callCount = 0;
+        process.once('uncaughtException', function(err) {
+            callCount += 1;
+        })
+        setTimeout(function(){ killSelf('SIGINT') }, 1);
+        setTimeout(function(){ killSelf('SIGINT') }, 2);
+        setTimeout(function(){
+            assert(callCount == 1);
+            done();
+        }, 20);
+    },
+
+    // should time box handler
+    function(done) {
+console.log("AR: INT timeout test, ts", Date.now());
+        qerror.reset();
+        qerror.timeout = 5;
+        qerror.handler = function(err, cb) {
+            setTimeout(cb, 10);
+        }
+        process.once('uncaughtException', function(err) {
+            qerror.uninstall();
+            assert(err.message.indexOf('qerror:') >= 0, "expected qerror: timeout error");
+console.log("AR: done with timeout test");
+// FIXME: if not delayed, below errors out wiht "expected same error rethrown"
+//            done();
+            setTimeout(done, 20);
+        })
+console.log("AR: killing to invoke sigint handler", process.listeners('SIGINT'));
+        // send the kill from a setTimeout, else bypasses the sig handler and kills
+        // the process if the signal is sent from inside a setImmediate
+        killSelf('SIGINT');
+    },
+
+    // should trap sighup
+    function(done) {
+console.log("AR: HUP trap test, ts", Date.now());
+        qerror.reset();
+console.log("AR:", qerror);
+        var err1;
+        qerror.handler = function(err, cb) {
+console.log("AR: got a HUP");
+            err1 = err;
+            assert.equal(err.message, 'SIGHUP', "expected HUP");
+            cb();
+        }
+console.log("AR: mark", process.listeners('SIGHUP'));
+        process.once('uncaughtException', function(err2) {
+console.log("AR: uncaught");
+            qerror.uninstall();
+            assert.equal(err1, err2, "expected same error rethrown");
+console.log("AR: done with test");
+            done();
+        })
+console.log("AR: about to hup");
+        killSelf('SIGHUP');
+    },
+
+    // should trap sigterm
+    function(done) {
+        qerror.reset();
+        var err1;
+        qerror.handler = function(err, cb) {
+            err1 = err;
+            cb();
+        }
+        process.once('uncaughtException', function(err2) {
+console.log("AR: err", err2);
+            assert.equal(err1.message, 'SIGTERM', "expected SIGTERM error");
+            assert.equal(err1, err2, "expected same error rethrown");
+            done();
+        })
+        killSelf('SIGTERM');
+    },
+
+    function(done) {
+        // wait for output to show up
+        setTimeout(done, 20);
+    },
+];
+
+var funcs = testFuncs;
+var funcIdx = 0;
+(function _iterate(){
+console.log("AR: iterate:");
+    var err;
+    if (funcIdx < funcs.length) {
+        funcs[funcIdx++](function(e) {
+console.log("AR: test func back", funcIdx);
+            err = e;
+            /*
+             * NOTE: this test works under node v0.8 and v0.10, but in node v4 and up
+             * (eg v6.10.2) the kill is not caught and delivered to the signal handler
+             * if killed from inside a setImmediate.  Works if the loop is iterated with
+             * setTimeout, oddly enough.  The kill is delivered if sent from a setTimeout.
+             */
+            setImmediate(_iterate);
+            //setTimeout(_iterate, 1);
+        })
+    }
+    else {
+        console.log("AR: Done.");
+    }
+})();
